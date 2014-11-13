50dd257bc4b7f22568a9b3c7eba96d919013b1ce
Daniel V <daniel.vernberg@gmail.com>
2014-11-13 14:01:35 +0100

    Added a link to the repo on GitHub in my footer

--- app/view/me/footer.tpl.php
+++ app/view/me/footer.tpl.php
@@ -1,2 +1,2 @@
-<footer><span class='sitefooter'>Copyright (c) Daniel | <a href='http://validator.w3.org/unicorn/check?ucn_uri=referer&amp;ucn_task=conformance'>Unicorn</a></span>
+<footer><span class='sitefooter'>Copyright (c) Daniel | <a href='https://github.com/danielv14/Anax-MVC'>GitHub</a>  | <a href='http://validator.w3.org/unicorn/check?ucn_uri=referer&amp;ucn_task=conformance'>Unicorn</a></span>
 </footer>
