3fe6b04c8794d7d34c62bac17929fa7ea64cf879
Daniel V <daniel.vernberg@gmail.com>
2014-11-29 19:05:15 +0100

    remote mysql config
    changed from localhost to remote in database_mysql.php

--- app/config/database_mysql.php
+++ app/config/database_mysql.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'dsn'     => "mysql:host=localhost;dbname=user;",
-    'username'        => "root",
-    'password'        => "root",
+   'dsn'     => "mysql:host=blu-ray.student.bth.se;dbname=dave14;",
+    'username'        => "dave14",
+    'password'        => "9En:2aR=",
     'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
     'table_prefix'    => "test_",
     'verbose' => false,
